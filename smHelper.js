40c061dac5b15293795398f678a210ad06db01ea
Sabit Igde <sabitertan@gmail.com>
2018-03-09 10:55:31 -0500

    add missing prototypes

--- smHelper.js
+++ smHelper.js
@@ -1,3 +1,35 @@
+/*Prototypes*/
+// check if an element exists in array using a comparer function
+// comparer : function(currentElement)
+Array.prototype.inArray = function(comparer) {
+  for(var i=0; i < this.length; i++) {
+    if(comparer(this[i])) return true;
+  }
+  return false;
+};
+
+// adds an element to the array if it does not already exist using a comparer
+// function
+Array.prototype.pushIfNotExist = function(element, comparer) {
+  if (!this.inArray(comparer)) {
+    this.push(element);
+  }
+};
+// removes an element to the array if it exists using a comparer
+// function
+Array.prototype.removeIfExist = function(element, comparer) {
+  if (this.inArray(comparer)) {
+    var index = this.indexOf(element);
+    if (index > -1) {
+      this.splice(index, 1);
+    }
+  }
+};
+// Finds index of an object key with giving value
+Array.prototype.findKeyIndex = function(key,value){
+  return this.map(function(item){return item[key];}).indexOf(value);
+};
+
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     define(['exports'], function(exports) {
@@ -252,7 +284,6 @@ exports.contentObserve = function contentObserve(targetId, mutationList) {
   var observerPos = (function () {
     return smHelper.observerPos;
   })();
-  var mutationCallback = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : function () {};
   if (window.navigator.userAgent.indexOf('Trident/') != -1) {
     document.addEventListener("DOMNodeInserted", function (e) {
       var mutationKey = mutationList.findKeyIndex('mutationId', e.target.id);
